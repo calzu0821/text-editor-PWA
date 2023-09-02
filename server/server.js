33ed050d277cbb2b9e9c2834690f3bdcaa77d4e8
Cristina Alzuri <cristinaalzuri@gmail.com>
2023-09-01 21:47:21 -0400

    imported the 'path' module

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+//import the 'path' module
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../client/dist'));
+// Serve static files for the 'client/dist' directory
+app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
